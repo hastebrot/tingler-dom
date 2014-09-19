eee6feda72ac46193e1d357ecbe960f57eb7bef0
hastebrot <hastebrot@gmail.com>
2014-09-19 10:26:29 +0200

    feat(): Add tingler controls.

--- src/tingler.js
+++ src/tingler.js
@@ -12,67 +12,52 @@ var stripIndent = function(str) {
   return indent > 0 ? str.replace(re, "") : str;
 };
 
-$(function() {
+var calculateTingleBounds = function($target) {
+  var position = $target.position();
 
-  var currentTarget = $("section");
+  var bounds = {
+    left: position.left,
+    top: position.top,
+    width: $target.outerWidth(),
+    height: $target.outerHeight()
+  };
 
-  var tinglerHighlight = $("<div>").addClass("tingler").addClass("highlight");
-  var tinglerPath = $("<div>").addClass("tingler").addClass("path");
-  $("body").append(tinglerHighlight);
-  $("body").append(tinglerPath);
+  if ($target[0].getBBox) {
+    var bbox = $target[0].getBoundingClientRect();
+    //var bbox = $target[0].getBBox();
+    bounds.width = bbox.width;
+    bounds.height = bbox.height;
+  }
 
-  // TODO: track previous $target to prevent repainting no-changes.
+  return bounds;
+}
 
-  currentTarget.on("mousemove", function(event) {
-    // only visible elements
-    // only top elements
-    // issues with <a> in map (zero dimensions)
-    // issues with "a" in formula (includes margins)
-    // isuues with wrapped <u> in text (different to firefox inspector)
-    // paths in map are difficult to pick
-    // issues with <tspan> in formula when zoomed (wrong dimensions)
+var updateTinglerHighlight = function($target, $tinglerHighlight) {
+  var bounds = calculateTingleBounds($target);
 
-    var $target = $(event.target);
+  $tinglerHighlight.css("left", bounds.left);
+  $tinglerHighlight.css("top", bounds.top);
+  $tinglerHighlight.css("width", bounds.width);
+  $tinglerHighlight.css("height", bounds.height);
+}
 
-    var tagName = $target.prop("tagName").toLowerCase();
-    var parentTagNames = _($target.parents()).map(function(parent) {
-      return parent.tagName.toLowerCase()
-    });
-    var position = $target.position();
-
-    var bbox = {
-      left: position.left,
-      top: position.top,
-      width: $target.outerWidth(),
-      height: $target.outerHeight()
-    };
-    if ($target[0].getBBox) {
-      var bounds = $target[0].getBoundingClientRect();
-      //var bounds = $target[0].getBBox();
-      bbox.width = bounds.width;
-      bbox.height = bounds.height;
-    }
-
-    tinglerHighlight.css("left", bbox.left);
-    tinglerHighlight.css("top", bbox.top);
-    tinglerHighlight.css("width", bbox.width);
-    tinglerHighlight.css("height", bbox.height);
-
-
-    tinglerPath.text(tagName + " < " + parentTagNames);
-    var position = {
-      left: event.pageX - (tinglerPath.outerWidth() / 2),
-      top: event.pageY - (tinglerPath.outerHeight() + 5)
-    };
-    var position = {
-      left: (bbox.left + (bbox.width / 2)) - (tinglerPath.outerWidth() / 2),
-      top: bbox.top - tinglerPath.outerHeight() - 5
-    };
-    tinglerPath.css("left", position.left).css("top", position.top);
-
-    //event.stopPropagation();
+var updateTinglerPath = function($target, $tinglerPath) {
+  var tagName = $target.prop("tagName").toLowerCase();
+  var parentTagNames = _($target.parents()).map(function(parent) {
+    return parent.tagName.toLowerCase()
   });
+  $tinglerPath.text(tagName + " < " + parentTagNames);
 
+  var bounds = calculateTingleBounds($target);
+  var position = {
+    left: (bounds.left + (bounds.width / 2)) - ($tinglerPath.outerWidth() / 2),
+    top: bounds.top - $tinglerPath.outerHeight() - 5
+  };
+  $tinglerPath.css("left", position.left).css("top", position.top);
+};
+
+
+var initSections = function() {
   // math formula.
   var $mathFormula = $("#math-formula .formula");
   katex.render($mathFormula.text(), $mathFormula[0]);
@@ -106,14 +91,56 @@ $(function() {
   $.getJSON("data/ne_110m_admin_0_countries.geo.json", function(data) {
     L.geoJson(data, {
       style: {
-          fillColor: "#fff",
-          fillOpacity: 1,
-          fill: true,
-          stroke: true,
-          color: "red",
-          weight: 2
-        }
+        fillColor: "#fff",
+        fillOpacity: 1,
+        fill: true,
+        stroke: true,
+        color: "red",
+        weight: 2
+      }
     }).addTo(map);
   });
+};
 
+var initTingler = function() {
+  var $tingleButtons = $(".annotate.button");
+  $tingleButtons.on("click", function() {
+    var $tingleButton = $(this);
+    var isActive = $tingleButton.hasClass("active");
+
+    $tingleButtons.removeClass("active");
+    if (!isActive) {
+      $tingleButton.addClass("active");
+    }
+  });
+
+  // only visible elements
+  // only top elements
+  // issues with <a> in map (zero dimensions)
+  // issues with "a" in formula (includes margins)
+  // isuues with wrapped <u> in text (different to firefox inspector)
+  // paths in map are difficult to pick
+  // issues with <tspan> in formula when zoomed (wrong dimensions)
+
+  var currentTarget = $("section");
+
+  var tinglerHighlight = $("<div>").addClass("tingler").addClass("highlight");
+  var tinglerPath = $("<div>").addClass("tingler").addClass("path");
+  $("body").append(tinglerHighlight);
+  $("body").append(tinglerPath);
+
+  // TODO: track previous $target to prevent repainting no-changes.
+
+  currentTarget.on("mousemove", function(event) {
+    var $target = $(event.target);
+
+    updateTinglerHighlight($target, tinglerHighlight);
+    updateTinglerPath($target, tinglerPath);
+    //event.stopPropagation();
+  });
+};
+
+$(function() {
+  initSections();
+  initTingler();
 });
