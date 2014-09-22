ad6f3b6de25fdaf0a7fe8bd2ed341ee19ee010b4
hastebrot <hastebrot@gmail.com>
2014-09-22 12:12:53 +0200

    improve(): Add source editor to every demo section.

--- src/demo.js
+++ src/demo.js
@@ -1,86 +1,80 @@
 
 var initSections = function() {
-  // markdown text.
-  var $markdownText = $("#text-comments pre code");
-  $markdownText.text(stripIndent($markdownText.html()).trim());
-  var $markdownHtml = $("#text-comments .markdown");
-  $markdownHtml.html(marked($markdownText.text(), {sanitize: false}));
 
-  // math formula.
-  var $mathFormula = $("#math-formula .formula");
-  katex.render($mathFormula.text(), $mathFormula[0]);
+  // MARKDOWN TEXT.
 
-  // graph network.
-  var $graphModel = $("#graph-network .model");
-  var $graphView = $("#graph-network svg g");
-  var model = graphlibDot.parse($graphModel.text());
-  var renderer = new dagreD3.Renderer();
-  var layout = dagreD3.layout().nodeSep(20).rankDir("LR");
-  renderer.layout(layout).run(model, d3.select($graphView.get(0)));
+  var $markdownText = $("#markdown-text");
+  var $markdownTextSource = $markdownText.find(".tingler-editor pre code");
+  var $markdownTextTarget = $markdownText.find(".tingler-target .markdown");
 
-  // source code.
-  var $sourceCode = $("#source-code pre code");
-  $sourceCode.text(stripIndent($sourceCode.text()).trim());
-  hljs.highlightBlock($sourceCode.get(0));
-  var sourceCodeLines = $sourceCode.html().split(/\n/);
-  $sourceCode.empty();
+  $markdownTextSource.text(sanitizeText($markdownTextSource.html()));
+  $markdownTextTarget.html(marked($markdownTextSource.text(), {sanitize: false}));
+
+  // LATEX FORMULA.
+
+  var $latexFormula = $("#latex-formula");
+  var $latexFormulaSource = $latexFormula.find(".tingler-editor pre code");
+  var $latexFormulaTarget = $latexFormula.find(".tingler-target .formula");
+
+  $latexFormulaSource.text(sanitizeText($latexFormulaSource.text()));
+  katex.render($latexFormulaSource.text(), $latexFormulaTarget.get(0));
+
+  // SOURCE CODE.
+
+  var $sourceCode = $("#source-code");
+  var $sourceCodeSource = $sourceCode.find(".tingler-editor pre code");
+  var $sourceCodeTarget = $sourceCode.find(".tingler-target pre code");
+
+  $sourceCodeSource.text(sanitizeText($sourceCodeSource.text()));
+  $sourceCodeTarget.text($sourceCodeSource.text());
+
+  hljs.highlightBlock($sourceCodeTarget.get(0));
+  var sourceCodeLines = $sourceCodeTarget.html().split(/\n/);
+  $sourceCodeTarget.empty();
   _(sourceCodeLines).each(function(sourceCodeLine, index) {
     var lineNumber = index + 1;
-    $line = $("<div>").attr("id", "line-" + lineNumber).html(sourceCodeLine);
-    $sourceCode.append($line);
+    var $line = $("<div>").attr("id", "line-" + lineNumber).html(sourceCodeLine);
+    $sourceCodeTarget.append($line);
   });
 
-  // geo map.
-  var $mapModel = $("#world-map .model");
-  var $mapView = $("#world-map .map");
-  var map = L.map($mapView.get(0), {attributionControl: false});
-  map.setView([49, 12], 4);
-  var defaultStyle = {
-    fillColor: "#fff",
-    fillOpacity: 1,
-    fill: true,
-    stroke: true,
-    color: "red",
-    weight: 2
-  };
-  $.getJSON($mapModel.text().trim(), function(data) {
+  // GRAPHVIZ GRAPH.
+
+  var $graphvizGraph = $("#graphviz-graph");
+  var $graphvizGraphSource = $graphvizGraph.find(".tingler-editor pre code");
+  var $graphvizGraphTarget = $graphvizGraph.find(".tingler-target svg g");
+
+  $graphvizGraphSource.text(sanitizeText($graphvizGraphSource.text()));
+
+  var model = graphlibDot.parse($graphvizGraphSource.text());
+  var renderer = new dagreD3.Renderer();
+  var layout = dagreD3.layout().nodeSep(20).rankDir("LR");
+  renderer.layout(layout).run(model, d3.select($graphvizGraphTarget.get(0)));
+
+  // GEOJSON MAP.
+
+  var $geojsonMap = $("#geojson-map");
+  var $geojsonMapSource = $geojsonMap.find(".tingler-editor pre code");
+  var $geojsonMapTarget = $geojsonMap.find(".tingler-target .map");
+
+  $geojsonMapSource.text(sanitizeText($geojsonMapSource.text()));
+
+  var leafletMap = L.map($geojsonMapTarget.get(0), {attributionControl: false});
+  leafletMap.setView([49, 12], 4);
+
+  $.getJSON($geojsonMapSource.text(), function(data) {
+    var defaultStyle = {
+      fillColor: "#fff",
+      fillOpacity: 1,
+      fill: true,
+      stroke: true,
+      color: "red",
+      weight: 2
+    };
     L.geoJson(data, {
       style: defaultStyle,
       onEachFeature: function(feature, layer) {}
-    }).addTo(map);
+    }).addTo(leafletMap);
   });
-
-  // pdf document.
-  // var $pdfView = $("#pdf-document .view");
-  // var $textLayer = $("#pdf-document .textLayer");
-  // var url = "data/TestDocument.pdf";
-  // PDFJS.getDocument(url).then(function(pdf) {
-  //   pdf.getPage(1).then(function(page) {
-  //     var scale = 1.5;
-  //     var viewport = page.getViewport(scale);
-
-  //     var canvas = $pdfView.get(0);
-  //     var context = canvas.getContext("2d");
-  //     canvas.height = viewport.height;
-  //     canvas.width = viewport.width;
-
-  //     var renderContext = {
-  //       canvasContext: context,
-  //       viewport: viewport
-  //     };
-  //     page.render(renderContext);
-
-  //     page.getTextContent().then(function (textContent) {
-  //       var textLayerBuilder = new TextLayerBuilder({
-  //         textLayerDiv: $textLayer.get(0),
-  //         viewport: viewport,
-  //         pageIndex: 0
-  //       });
-  //       textLayerBuilder.setTextContent(textContent);
-  //     });
-  //   });
-  // });
-
 };
 
 // https://github.com/sindresorhus/strip-indent
@@ -94,6 +88,10 @@ var stripIndent = function(str) {
   }));
   var re = new RegExp("^[ \\t]{" + indent + "}", "gm");
   return indent > 0 ? str.replace(re, "") : str;
+};
+
+var sanitizeText = function(text) {
+  return stripIndent(text).trim();
 };
 
 $(function() {
